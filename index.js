597e4b70554a62d90c7f68e9d0d227a3c9c55127
PlainDevelopment <66025383+PlainDevelopment@users.noreply.github.com>
2021-12-15 22:06:25 +0000

    Bug fix

--- index.js
+++ index.js
@@ -282,41 +282,35 @@ class Economy {
      */
     static async leaderboard(type) {
 
-        if (!type) {
+        if (!type || type === "both") {
             const d2 = await require('./db.js').find({});
-
+            if(!d2) return;
             d2.sort(function (a, b) {
-                return a.balance.wallet - b.balance.wallet;
+                if(a.balance.bank + a.balance.wallet === 0 || b.balance.bank + b.balance.wallet=== 0) return;
+                return (a.balance.bank + a.balance.wallet) - (b.balance.bank + b.balance.wallet);
             });
-            if (!d2[0] || d2[0].balance.wallet === 0) return false;
+            if (!d2[0]) return false;
+
             return d2;
         }
         if (type === "wallet") {
             const d2 = await require('./db.js').find({});
-
+            if(!d2) return;
             d2.sort(function (a, b) {
+                if(a.balance.wallet === 0 || b.balance.wallet=== 0) return;
                 return a.balance.wallet - b.balance.wallet;
             });
-            if (!d2[0] || d2[0].balance.wallet === 0) return false;
+            if (!d2[0]) return false;
             return d2;
         }
         if (type === "bank") {
             const d2 = await require('./db.js').find({});
-
+            if(!d2) return;
             d2.sort(function (a, b) {
+                if(a.balance.bank === 0 || b.balance.bank=== 0) return;
                 return a.balance.bank - b.balance.bank;
             });
-            if (!d2[0] || d2[0].balance.bank === 0) return false;
-            return d2;
-        }
-        if (type === "both") {
-            const d2 = await require('./db.js').find({});
-
-            d2.sort(function (a, b) {
-                return (a.balance.bank + a.balance.wallet) - (b.balance.bank + b.balance.bank);
-            });
             if (!d2[0]) return false;
-            if (d2[0].balance.bank === 0 && d2[0].balance.wallet === 0) return false;
             return d2;
         }
     }
